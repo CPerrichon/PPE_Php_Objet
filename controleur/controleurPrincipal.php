cbeaf1e0f47cf43b79ab43a678e0747357f751f9
CPerrichon <62330243+CPerrichon@users.noreply.github.com>
2021-05-24 14:48:59 +0200

    modif controleur

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -8,6 +8,7 @@ function controleurPrincipal($action) {
     $lesActions["logout"]="LogoutControleur.php";
     $lesActions["creer"]="creerPoste.php";
     $lesActions["modif"]="modifierPoste.php";
+    
     if (array_key_exists($action, $lesActions)) {
         return $lesActions[$action];
     } else {
